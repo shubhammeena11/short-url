99204931ee3fa651f80240a3df51c3358976a733
shubhammeena11 <shubhammeena982@gmail.com>
2025-02-01 01:49:06 +0530

    changing PORT to 10000

--- short-url-front/src/components/UrlForm.jsx
+++ short-url-front/src/components/UrlForm.jsx
@@ -1,5 +1,5 @@
 
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { PortContext } from '../PortContext';
 
 function UrlForm() {
