e33688cbc73574bf23363d7a701826fa0d064107
shubhammeena11 <shubhammeena982@gmail.com>
2025-02-02 03:06:11 +0530

    final commit

--- short-url/controllers/user.js
+++ short-url/controllers/user.js
@@ -1,27 +1,28 @@
-const User = require("../models/user")
+const User = require("../models/user");
 
-async function createUser(req, res){
-    const {name, email, password} = req.body; 
-    await User.create({
-        name : name,
-        email : email,
-        password : password,
-    })
-    res.redirect("/app")
-}
-
-async function logoinUser(req, res){
-    const {email, password} = req.body; 
-    const user = await User.findOne({email, password});
-    if(!user){
-        return res.render("/login",{
-            error : 'Invalid Username or Password',
-        })
+async function createUser(req, res) {
+    try {
+        const { name, email, password } = req.body;
+        const newUser = await User.create({ name, email, password });
+        res.status(201).json({ message: "User created successfully", user: newUser });
+    } catch (error) {
+        res.status(500).json({ message: "Error creating user", error });
     }
-    res.redirect("/app")
 }
 
-module.exports ={
-    createUser,
-    logoinUser
+async function loginUser(req, res) { // Fix function name
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email, password });
+
+        if (!user) {
+            return res.status(401).json({ message: "Invalid Username or Password" });
+        }
+
+        res.status(200).json({ message: "Login successful", user });
+    } catch (error) {
+        res.status(500).json({ message: "Error logging in", error });
+    }
 }
+
+module.exports = { createUser, loginUser }; // Fix export name
